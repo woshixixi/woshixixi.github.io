b5800945c70192defa6a4e5f3ceedf8984aa5b2d
czhang <czhang@yuansuan.cn>
2019-05-10 15:39:45 +0800

    add promise.then

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # woshixixi.github.io
 
 1. [Cross Domain](https://github.com/woshixixi/woshixixi.github.io/blob/master/cross-domain/README.md)
+
+2. [Promise.then]()
---
+++ src/es6/Promise.js
@@ -0,0 +1,61 @@
+class PromiseA {
+  constructor(executor) {
+    this.state = 'pending'
+    this.value = undefined
+    this.reason = undefined
+    this.onFulfilledCallback = []
+    this.onRejectedCallback = []
+
+    let resolve = v => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled'
+        this.value = v
+
+        this.onFulfilledCallback.forEach(fn => fn())
+      }
+    }
+
+    let reject = v => {
+      if (this.state === 'pending') {
+        this.state = 'rejected'
+        this.reason = v
+
+        this.onRejectedCallback.forEach(fn => fn())
+      }
+    }
+
+    try {
+      executor(resolve, reject)
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  thenA(onFulfilled, onRejected) {
+    if (this.state === 'fulfilled') {
+      onFulfilled(this.value)
+    }
+
+    if (this.state === 'rejected') {
+      onRejected(this.reason)
+    }
+
+    if (this.state === 'pending') {
+      this.onFulfilledCallback.push(() => onFulfilled(this.value))
+      this.onRejectedCallback.push(() => onRejected(this.reason))
+    }
+  }
+}
+
+// test
+const p = new PromiseA((res, rej) => {
+  console.log('....')
+  setTimeout(() => {
+    res('a')
+    console.log('res ...p:', p)
+  }, 2000)
+})
+
+p.thenA(res => {
+  console.log('then...', res)
+})
